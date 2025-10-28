c464065f4f0bacb07ed85b5046d74e39396ed505
klein0102 <a.tony200301@gmail.com>
2025-10-28 15:01:30 -0600

    Add files via upload

---
+++ Controllers/ListasController.cs
@@ -0,0 +1,60 @@
+﻿using ListaDeCompras.BC.Entidades;
+using ListaDeCompras.BW.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaDeCompras.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ListasController : ControllerBase
+    {
+        private readonly IGestionListaBW _gestionListaBW;
+
+        public ListasController(IGestionListaBW gestionListaBW)
+        {
+            _gestionListaBW = gestionListaBW;
+        }
+
+        // GET: api/Listas
+        [HttpGet]
+        public IActionResult ObtenerListas()
+        {
+            var listas = _gestionListaBW.ObtenerListas();
+            return Ok(listas);
+        }
+
+        // GET: api/Listas/{id}
+        [HttpGet("{id}")]
+        public IActionResult ObtenerListaPorId(Guid id)
+        {
+            var lista = _gestionListaBW.ObtenerListaPorId(id);
+            if (lista == null)
+                return NotFound("Lista no encontrada.");
+
+            return Ok(lista);
+        }
+
+        // POST: api/Listas
+        [HttpPost]
+        public IActionResult CrearLista([FromBody] ListaCompra lista)
+        {
+            try
+            {
+                _gestionListaBW.CrearLista(lista);
+                return Ok("Lista creada correctamente.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE: api/Listas/{id}
+        [HttpDelete("{id}")]
+        public IActionResult EliminarLista(Guid id)
+        {
+            _gestionListaBW.EliminarLista(id);
+            return Ok("Lista eliminada correctamente.");
+        }
+    }
+}
